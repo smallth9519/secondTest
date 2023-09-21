e48c7b1233f8d5f13dcf1fecdda8a1c8681d958b
th9519 <th9519@gmail.com>
2023-09-21 12:41:45 +0900

    받아

--- src/test/Test1.java
+++ src/test/Test1.java
@@ -3,5 +3,9 @@ package test;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println(1);
+        System.out.println(1);
+        System.out.println(1);
+        System.out.println(1);
+        System.out.println(1);
     }
 }
